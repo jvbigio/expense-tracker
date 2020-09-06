c73304523c5dd65678f7a0f25bf436edf906a7a3
Joe Bigio <jvbigio@gmail.com>
2020-09-06 14:45:55 -0400

    Working on button submit capture data

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log(123)
+const form = document.querySelector('.add-form')
+const date = document.getElementById('date')
+// console.log(date)
+form.addEventListener('submit', addItem)
+
+function addItem (e) {
+  e.preventDefault()
+  const dataArray = []
+  dataArray.push(date.value) // works on submit
+  console.log(dataArray)
+  // console.log('clicked')
+  // document.addEventListener('change', function (e) {
+  //   console.log(e.target.value)
+  // })
+  // e.date.value
+  // date.addEventListener('change', event => {
+  //   console.log(event.target.value)
+  // })
+  // document.getElementById('date').addEventListener('change', function (event) {
+  //   console.log(event.target.value)
+  // })
+}
