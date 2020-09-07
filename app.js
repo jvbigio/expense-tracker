f698386c6efb7a0f6e23cabe725455f7829200a1
Joe Bigio <jvbigio@gmail.com>
2020-09-07 15:29:04 -0400

    Working on localStorage

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 const form = document.querySelector('.add-form')
 const date = document.getElementById('date')
+// const description = document.getElementById('description').value
+// const category = document.getElementById('category')
+// const amount = document.getElementById('amount')
 // console.log(date)
 form.addEventListener('submit', addItem)
 
 function addItem (e) {
+  const expenseDate = date.value
+  // const expenseDescription = description
+  // const expenseCategory = category.value
+  // const expenseAmount = amount.value
+  const description = document.getElementById('description').value
+  const category = document.getElementById('category').value
+  const amount = document.getElementById('amount').value
   e.preventDefault()
   const dataArray = []
-  dataArray.push(date.value) // works on submit
+  dataArray.push(date.value, description, category, amount) // works on submit
+  // dataArray.push(description)
   console.log(dataArray)
-  // console.log('clicked')
-  // document.addEventListener('change', function (e) {
-  //   console.log(e.target.value)
-  // })
-  // e.date.value
-  // date.addEventListener('change', event => {
-  //   console.log(event.target.value)
-  // })
-  // document.getElementById('date').addEventListener('change', function (event) {
-  //   console.log(event.target.value)
-  // })
+  dataSaver(dataArray)
+  console.log(dataSaver)
+  date.value = ''
+  description.value = ''
+  category.value = ''
+  amount.value = ''
+}
+
+// dataSet is the plural set of data, data is the single
+function dataSaver (dataArray) {
+  const dataSet = dataStorageHelper()
+  dataSet.push(dataArray)
+  localStorage.setItem('dataSet', JSON.stringify(dataSet))
+}
+
+// function dataLoader () {
+//   const dataSet = dataStorageHelper()
+//   dataSet.forEach(data => {
+
+//   })
+// }
+
+function dataStorageHelper () {
+  const dataGetter = JSON.parse(localStorage.getItem('dataSet'))
+  let dataSet
+  if (!dataGetter) {
+    dataSet = []
+  } else {
+    dataSet = JSON.parse(localStorage.getItem('dataSet'))
+  }
+  return dataSet
 }
